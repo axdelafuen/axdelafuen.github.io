e2ee328ecf9090823e11a23e54dbec0f0d83e61f
axdelafuen <axel.dlf@free.fr>
2022-06-03 18:26:07 +0200

    modification du clignotement page accueil

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-
+var clignotement = function(){
+   if (document.getElementById('blink').style.visibility=='visible'){
+      document.getElementById('blink').style.visibility='hidden';
+   }
+   else{
+   document.getElementById('blink').style.visibility='visible';
+   }
+};
+periode = setInterval(clignotement, 400);
