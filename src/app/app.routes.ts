a7102fa5173411d2ed9d641d98c743e356c7f65c
axdelafuen <axel.dlf63@gmail.com>
2025-09-02 13:09:37 +0000

    add pages exp and cv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,5 +8,13 @@ export const routes: Routes = [
   {
     path: 'projects',
     loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent)
+  },
+  {
+    path: 'experiences',
+    loadComponent: () => import('./pages/experiences/experiences.component').then(m => m.ExperiencesComponent)
+  },
+  {
+    path: 'download-cv',
+    loadComponent: () => import('./pages/download-cv/download-cv.component').then(m => m.DownloadCvComponent)
   }
 ];
